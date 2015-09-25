22d26729ee5981c14eda1fafc340da80775ec2e6
Steve Janzou <steven.janzou@nrel.gov>
2015-09-25 07:32:25 +0000

    pv_shade_db branch - add simplified compute module cmod_pv_get_shade_loss_mpp.cpp and match with matlab results.

--- ssc/sscapi.cpp
+++ ssc/sscapi.cpp
@@ -80,6 +80,7 @@ extern module_entry_info
 	cm_entry_battery,
 	cm_entry_test_pv_shade_loss_db,
 	cm_entry_pv_get_shade_loss,
+	cm_entry_pv_get_shade_loss_mpp,
 	cm_entry_pv_get_single_shade_loss,
 	cm_entry_lcoefcr;
 
@@ -143,6 +144,7 @@ static module_entry_info *module_table[] = {
 	&cm_entry_battery,
 	&cm_entry_test_pv_shade_loss_db,
 	&cm_entry_pv_get_shade_loss,
+	&cm_entry_pv_get_shade_loss_mpp,
 	&cm_entry_pv_get_single_shade_loss,
 	&cm_entry_lcoefcr,
 	0 };
