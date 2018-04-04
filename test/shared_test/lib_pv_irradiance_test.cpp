bd9343e8f561d6695f06047aad75302cfdf835b8
nickdiorio <nadiorio@gmail.com>
2018-04-03 21:12:31 -0600

    Example of updating test

--- test/shared_test/lib_pv_irradiance_test.cpp
+++ test/shared_test/lib_pv_irradiance_test.cpp
@@ -3,6 +3,13 @@
 
 TEST_F(LibPVIrradianceTest, DefaultNoFinancialModel) {
 
+	// Run with defaults for a step
 	EXPECT_EQ(irradianceModel->RunSingleStep(0), EXIT_SUCCESS);
+
+	// Update sky model and run for a step
+	PVIO->getIrradianceIO()->skyModel = 0;
+	EXPECT_EQ(irradianceModel->RunSingleStep(0), EXIT_SUCCESS);
+
+
 }
 
