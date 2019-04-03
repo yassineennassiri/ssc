3ab35cba6ad2de0eb6d2e2086898907ff969617e
yassineennassiri <36824782+yassineennassiri@users.noreply.github.com>
2019-04-03 12:18:46 +0100

    Update lib_alkaline_electrolyzer.h

--- shared/lib_alkaline_electrolyzer.h
+++ shared/lib_alkaline_electrolyzer.h
@@ -4,3 +4,31 @@
 #include <map>
 #include "lib_util.h"
 
+/** 
+* \ class AlkalineElectrolyzer
+*
+* \brief
+*
+*  The AlkalineElectrolyzer class provides the overall proporties and operation of the electrolyzer to model 
+   the Alkaline electrolyzer technology in SAM.
+*/
+
+class AlkalineElectrolyzer
+{
+  public:
+          /// Default AlkalineElectrolyzer constructor
+          AlkalineElectrolyzer();
+  
+          /// Construct Alkaline Electrolyzer arguments
+          AlkalineElectrolyzer('arguments') ;
+  
+  
+          /// Default destructor
+          ~AlkalineElectrolyzer();
+  
+  
+          /// Copy Destructor 
+          AlkalineElectrolyzer(const AlkalineElectrolyzer &alkalineElectrolyzer);
+  
+  
+          /// Run for single time step
