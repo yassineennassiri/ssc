b8b9353e3f1cfb3defe55677c0097ce5b65815c3
yassineennassiri <36824782+yassineennassiri@users.noreply.github.com>
2019-04-08 16:49:46 +0100

    Update lib_alkaline_electrolyzer_dispatch.h

--- shared/lib_alkaline_electrolyzer_dispatch.h
+++ shared/lib_alkaline_electrolyzer_dispatch.h
@@ -1,1 +1,30 @@
+#ifndef _LIB_ALKALINE_ELECTROLYZER_DISPATCH_
+#DEFINE _LIB_ALKALINE_ELECTROLYZER_DISPATCH_
 
+#include "lib_alkaline_electrolyzer.h"
+
+class AlkalineElectrolyzerDispatch
+{
+public:
+        /// Default constructor
+        AlkalineElectrolyzerDispatch() { /* Nothing to do */ };
+        
+        /// Construct with argument
+        AlkalineElectrolyzerDispatch();
+        
+        /// Destructor
+        ~AlkalineElectrolyzer();
+        
+        /// Run dispatch for single step
+        void runSingleTimeStep();
+        
+        /// Update dispatch units (for testing)
+        void setDispatchOption();
+        
+        /// Update the fixed percentage to dispatch
+        void setFixedDischargePercentage();
+        
+        /// Update dispatch units (for testing)
+        void seManualDispacthUnits();
+        
+        /// Get the total Hydrogen Produced kg
